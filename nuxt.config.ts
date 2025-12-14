ee502fece718c7437287b97de1a2e0b461640c3f
chibx <111882496+chibx@users.noreply.github.com>
2025-12-14 01:27:07 +0100

    Changed Nuxt Config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,29 @@
+import tailwind from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
-})
+  compatibilityDate: "2025-07-15",
+  css: ["~/assets/css/tailwind.css"],
+  devtools: { enabled: false },
+  components: {
+    dirs: [],
+  },
+  modules: ["@nuxt/eslint"],
+  vite: {
+    plugins: [tailwind()],
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component.
+     * @default "Ui"
+     */
+    prefix: "Ui",
+    /**
+     * Directory that the component lives in.
+     * Will respect the Nuxt aliases.
+     * @link https://nuxt.com/docs/api/nuxt-config#alias
+     * @default "@/components/ui"
+     */
+    componentDir: "@/components/ui",
+  },
+});
