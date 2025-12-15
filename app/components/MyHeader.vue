be2d75a27e5cc4befb98a6244d5406f8cb2ec94e
chibx <111882496+chibx@users.noreply.github.com>
2025-12-15 03:09:25 +0100

    New commit

---
+++ app/components/MyHeader.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { MenuIcon } from "lucide-vue-next";
+import YTLogo from "~/components/svg/YTLogo.vue";
+</script>
+
+<template>
+    <header class="h-[55px] flex gap-10 items-center">
+        <MenuIcon />
+        <YTLogo class="h-10 w-20 text-white" />
+    </header>
+</template>
+
+<style scoped></style>
