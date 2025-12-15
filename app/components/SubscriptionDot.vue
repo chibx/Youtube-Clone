2485f6c964becc426b0e5f89ca0ca04c535938b6
chibx <111882496+chibx@users.noreply.github.com>
2025-12-15 03:08:37 +0100

    Sidebar Live and New Content simple dot component

---
+++ app/components/SubscriptionDot.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = withDefaults(
+    defineProps<{
+        /** (px) */
+        height?: number;
+        /** (px) */
+        width?: number;
+        color?: string;
+    }>(),
+    {
+        color: "blue",
+        width: 10,
+        height: 10,
+    },
+);
+</script>
+
+<template>
+    <div class="dot" :style="{ backgroundColor: color, width: width, height: height }"></div>
+</template>
+
+<style scoped>
+.dot {
+    border-radius: 50%;
+}
+</style>
