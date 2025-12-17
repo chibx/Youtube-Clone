3189a376317b1594028ec1006f310ffe360860be
chibx <111882496+chibx@users.noreply.github.com>
2025-12-17 14:51:19 +0100

    Header done for now
    Going on to the next

--- app/components/MyHeader.vue
+++ app/components/MyHeader.vue
@@ -1,17 +1,98 @@
 <script setup lang="ts">
-import { MenuIcon } from "lucide-vue-next";
+import { AntennaIcon, PlusIcon, MenuIcon, MicIcon, NotebookIcon, SearchIcon, UploadIcon, BellIcon } from "lucide-vue-next";
 import YTLogo from "~/components/svg/YTLogo.vue";
+import { Input as UiInput } from "@/components/ui/input";
+import { Button as UiButton } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { initials } from "~/utils/utils";
+
+const searchText = ref("");
+const name = ref("Adam Jake")
 </script>
 
 <template>
-    <header class="fixed top-0 w-full h-[55px] flex gap-10 items-center">
-        <MenuIcon />
-        <YTLogo class="h-10 w-20" />
+    <header class="fixed px-5 top-0 w-full h-(--myheader-height) flex justify-between items-center">
+        <div class="flex gap-5 items-center">
+            <MenuIcon />
+            <YTLogo class="logo h-10 w-20" />
+        </div>
+
+        <div class="flex gap-5">
+            <div class="flex items-center">
+                <UiInput
+                    class="border border-white/20 rounded-tl-full rounded-bl-full min-w-[300px] md:min-w-[400px] py-5 px-5"
+                    placeholder="Search anything..." />
+                <UiButton
+                    class="cursor-pointer bg-gray-600 dark:bg-gray-500/30 border border-white/20 flex items-center justify-center rounded-tr-full rounded-br-full py-5 px-5!">
+                    <SearchIcon class="dark:text-white" />
+                </UiButton>
+            </div>
+
+            <div class="rounded-full p-2.5 bg-gray-600 dark:bg-gray-500/20">
+                <MicIcon />
+            </div>
+        </div>
+
+        <div class="flex gap-5">
+            <DropdownMenu>
+                <DropdownMenuTrigger as-child>
+                    <UiButton
+                        class="w-25 flex items-center gap-1.5 py-5 px-5 rounded-full cursor-pointer dark:bg-[#ffffff1a] dark:hover:bg-white/20 dark:text-white">
+                        <PlusIcon />
+                        Create
+                    </UiButton>
+                </DropdownMenuTrigger>
+
+                <DropdownMenuContent class="w-45 mr-5 py-2.5 rounded-xl">
+                    <DropdownMenuItem as-child>
+                        <NuxtLink to="#" class="cursor-pointer flex items-center gap-2.5 p-2.5 py-3">
+                            <UploadIcon />
+                            Upload
+                        </NuxtLink>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem as-child>
+                        <NuxtLink to="#" class="cursor-pointer flex items-center gap-2.5 p-2.5 py-3">
+                            <AntennaIcon />
+                            Go Live
+                        </NuxtLink>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem as-child>
+                        <NuxtLink to="#" class="cursor-pointer flex items-center gap-2.5 p-2.5 py-3">
+                            <NotebookIcon />
+                            Create Post
+                        </NuxtLink>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+
+            <UiButton class="rounded-full p-2.5 bg-gray-600 dark:bg-gray-500/20">
+                <BellIcon />
+            </UiButton>
+
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <Avatar>
+                        <AvatarImage src="/yt-thum.webp" />
+                        <AvatarFallback :style="{ backgroundColor: 'red', color: 'white' }">{{ initials(name) }}
+                        </AvatarFallback>
+                    </Avatar>
+                </DropdownMenuTrigger>
+
+                <DropdownMenuContent>
+
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
     </header>
 </template>
 
 <style scoped>
 .dark svg {
+    color: white;
+}
+
+.dark .logo {
     fill: white;
 }
 </style>
