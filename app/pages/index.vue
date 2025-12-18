7a14c1102062fc81ed6cfaea08403c16007b2e50
chibx <111882496+chibx@users.noreply.github.com>
2025-12-18 16:51:02 +0100

    Working on the category switcher that is meant to handle the switching of preferred videos on the home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { } from "@/components/ui/card"
+import CategorySwitcher from "~/components/homepage/CategorySwitcher.vue";
+
+</script>
 
 <template>
     <div>
+        <CategorySwitcher />
         <h1 class="text-2xl">Hello</h1>
     </div>
 </template>
