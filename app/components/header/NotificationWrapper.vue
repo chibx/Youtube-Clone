b1c275bc4c02ab885192404917dd56a4feea2700
chibx <111882496+chibx@users.noreply.github.com>
2025-12-18 16:31:41 +0100

    Added a new component for the notification dropdown

---
+++ app/components/header/NotificationWrapper.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { Button as UiButton } from "@/components/ui/button";
+import { BellIcon } from "lucide-vue-next";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import { Card, CardContent } from "~/components/ui/card"
+import { Spinner } from "~/components/ui/spinner";
+
+const isNotificationLoading = ref(true);
+</script>
+
+<template>
+    <DropdownMenu>
+
+        <DropdownMenuTrigger>
+            <UiButton
+                class="cursor-pointer rounded-full size-10 border border-gray-300 hover:bg-gray-300/40 dark:border-none text-black bg-white dark:bg-gray-500/20">
+                <BellIcon />
+            </UiButton>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent align="end" as-child>
+            <Card v-if="isNotificationLoading" class="w-[350px] min-h-[300px] flex items-center justify-center">
+                <CardContent class="flex items-center justify-center">
+                    <Spinner class="size-10" />
+                </CardContent>
+            </Card>
+        </DropdownMenuContent>
+    </DropdownMenu>
+</template>
+
+<style scoped>
+.dark svg {
+    color: white;
+}
+</style>
