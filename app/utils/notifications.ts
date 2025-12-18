e534d6fbf6de55095b3a7977a17f7398a6b38475
chibx <111882496+chibx@users.noreply.github.com>
2025-12-19 00:16:56 +0100

    Worked on mock data generation

--- app/utils/notifications.ts
+++ app/utils/notifications.ts
@@ -1,1 +1,97 @@
-export const mockNotifications = {}
+import { NotificationsType, type YTNotification } from "./types";
+
+const usernames = [
+    "StarryNights23",
+    "TechWanderer98",
+    "PixelPioneer22",
+    "SunnyDreamer17",
+    "EchoExplorer44",
+    "UrbanNomad12",
+    "CreativeSpark88",
+    "NatureLover06",
+    "ChillVibes2023",
+    "MysticJourney55",
+    "SkyHighAdventurer",
+    "ArtisticSoul09",
+    "GamerGuru77",
+    "BookwormBabe14",
+    "WanderlustSeeker1",
+    "CryptoNinja99",
+    "FitnessFanatic55",
+    "HealthyEaterX",
+    "MusicLover53",
+    "DreamBigAlways",
+];
+
+const fullNames = [
+    "Ella Johnson",
+    "Liam Williams",
+    "Sofia Martinez",
+    "Mason Brown",
+    "Chloe Garcia",
+    "Ethan Wilson",
+    "Ava Thompson",
+    "Noah Anderson",
+    "Isabella Taylor",
+    "Lucas Robinson",
+    "Zoe Scott",
+    "Aiden Davis",
+    "Mia Clark",
+    "Oliver Miller",
+    "Harper Young",
+    "Jack Harris",
+    "Amelia Lewis",
+    "James Walker",
+    "Charlotte Hall",
+    "Benjamin Allen",
+];
+
+const notifTypes = [NotificationsType.CommentLike, NotificationsType.CommentRep];
+
+export function mockNotifications(limit: number) {
+    const notifs: YTNotification[] = [];
+
+    for (let i = 0; i < limit; i++) {
+        const date = new Date();
+        // date.setHours(
+        //     Math.floor(Math.random() * 24),
+        //     Math.floor(Math.random() * 60),
+        //     Math.floor(Math.random() * 60)
+        // );
+
+        // if (Math.random() > 0.0001) {
+        //     date.setDate(date.getDate() - Math.floor(Math.random() * date.getDate()));
+        // }
+
+        if (Math.random() > 0.002) {
+            date.setHours(date.getHours() - Math.ceil(Math.random() * date.getHours()));
+        }
+
+        if (Math.random() > 0.3) {
+            date.setMinutes(date.getMinutes() - Math.ceil(Math.random() * date.getMinutes()));
+        }
+
+        if (Math.random() > 0.5) {
+            date.setSeconds(date.getSeconds() - Math.ceil(Math.random() * date.getSeconds()));
+        }
+
+        // console.log(date);
+
+        notifs.push({
+            date: date,
+            title: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt sunt molestias dolorum aliquam veritatis officia repellat nesciunt laborum consequuntur amet. Repellat accusamus dolores esse corrupti eveniet aliquam sunt, blanditiis quam.",
+            actor: usernames[Math.floor(usernames.length * Math.random())],
+            type: notifTypes[Math.floor(notifTypes.length * Math.random())] as NotificationsType,
+            avatarName: fullNames[Math.floor(fullNames.length * Math.random())],
+            avatarImg: Math.random() > 0.6 ? "/yt-thumb.webp" : undefined,
+            picture:
+                Math.random() > 0.1
+                    ? Math.random() > 0.5
+                        ? "/9x16.webp"
+                        : "16x9.webp"
+                    : undefined,
+        });
+    }
+
+    return notifs;
+}
--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -1,6 +1,53 @@
+import _dayjs from "dayjs";
+
 export function initials(name: string) {
-  return name
-    .split(" ")
-    .map((name) => name[0])
-    .join("");
+    return name
+        .split(" ")
+        .map((name) => name[0])
+        .join("");
+}
+
+export function delay(ms: number) {
+    return new Promise<void>((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+function convert(date: Date) {
+    const dayjs = _dayjs(date);
+    const nowjs = _dayjs(new Date());
+
+    const monthDiff = nowjs.diff(dayjs, "month");
+    const dayDiff = nowjs.diff(dayjs, "day");
+    const hoursDiff = nowjs.diff(dayjs, "hour");
+    const minsDiff = nowjs.diff(dayjs, "minute");
+    const secsDiff = nowjs.diff(dayjs, "second");
+
+    switch (true) {
+        case monthDiff > 0:
+            return `${monthDiff} month${monthDiff > 1 ? "s" : ""}`;
+        case dayDiff > 0:
+            return `${dayDiff} day${dayDiff > 1 ? "s" : ""}`;
+        case hoursDiff > 0:
+            return `${hoursDiff} hour${hoursDiff > 1 ? "s" : ""}`;
+        case minsDiff > 0:
+            return `${minsDiff} minute${minsDiff > 1 ? "s" : ""}`;
+        case secsDiff > 0:
+            return `${secsDiff} second${secsDiff > 1 ? "s" : ""}`;
+        default:
+            return "";
+    }
+}
+
+export function diffDate(date: Date | string): string {
+    if (typeof date == "object") {
+        return convert(date);
+    }
+
+    try {
+        const _date = new Date(date);
+        return convert(_date);
+    } catch {
+        return "";
+    }
 }
