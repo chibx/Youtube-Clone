ade53fd35da58a8b0589c4c54bd363335e44cba7
chibx <111882496+chibx@users.noreply.github.com>
2025-12-18 16:32:31 +0100

    Made some changes to the theme to render the class initially if the cookie is set

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import 'vue-sonner/style.css'
+import { Toaster } from "@/components/ui/sonner"
+import { useDark } from '@vueuse/core';
+
+const theme = useCookie("theme")
+
+const isDark = useDark({
+    storageKey: "theme",
+    // initOnMounted: false,
+    // listenToStorageChanges: false,
+    // writeDefaults: false,
+    onChanged(_, defaultHandler, mode) {
+        theme.value = mode == "auto" ? "" : (mode == "light" ? "light" : "dark")
+        defaultHandler(mode)
+    },
+});
+
+useHead({
+    htmlAttrs: {
+        class: () => theme.value == "dark" ? "dark" : undefined,
+    }
+})
+
+onMounted(() => {
+    // setTimeout(() => {
+    //     // isDark.value = false
+    //     console.log(isDark.value)
+    // }, 1000)
+})
+</script>
 
 <template>
+    <Toaster />
     <div class="bg-background">
         <NuxtRouteAnnouncer />
         <NuxtLayout>
@@ -14,6 +45,5 @@
     --myheader-height: 60px;
 }
 
-#__nuxt {
-}
+#__nuxt {}
 </style>
