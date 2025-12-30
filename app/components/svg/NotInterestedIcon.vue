ae9038d8e6f46fc3622d5be079ffe02ac6c17173
chibx <111882496+chibx@users.noreply.github.com>
2025-12-30 23:59:24 +0100

    New Commit

---
+++ app/components/svg/NotInterestedIcon.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false"
+        aria-hidden="true" style="pointer-events: none;">
+        <path
+            d="M12 1C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11S18.075 1 12 1Zm0 2a9 9 0 018.246 12.605L4.755 6.661A8.99 8.99 0 0112 3ZM3.754 8.393l15.491 8.944A9 9 0 013.754 8.393Z">
+        </path>
+    </svg>
+</template>
+
+<style scoped>
+.dark svg {
+    fill: #fff;
+}
+</style>
